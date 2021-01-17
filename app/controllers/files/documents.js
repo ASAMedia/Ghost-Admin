293abd072770242d19a50c4b4729fb2a90aca09f
Axel Schorcht <schorchtaxel@gmail.com>
2021-01-17 22:45:15 +0100

    Tried different methods

--- app/controllers/files/documents.js
+++ app/controllers/files/documents.js
@@ -1,56 +1,52 @@
 import Controller from '@ember/controller';
 import { get } from '@ember/object';
 import { task } from 'ember-concurrency';
-import {inject as service} from '@ember/service';
-import {run} from '@ember/runloop';
+import { inject as service } from '@ember/service';
+import { run } from '@ember/runloop';
 
 export default Controller.extend({
     ajax: service(),
 
-    imageExtensions: ['gif', 'jpg', 'jpeg', 'png', 'svg'],
     init() {
         this._super(...arguments);
     },
     actions: {
-        uploadImage(file) {
-            this.uploadPhoto(file);
-        }
-    },
-    async uploadPhoto(file) {
-        /* const response = await fetch(`${window.location.origin}/ghost/api/v3/admin/images/upload/`, {
-            method: 'post',
-            headers: {
-                'Content-Type': 'form-data;',
-                'file': ('ifxb.png', file, 'image/png'),
-                'params': {'purpose': 'image', 'ref': 'ifxb.png'}
-            }
-        }); */
-        let ajax = this.ajax;
-        let formData = new FormData();
+        async onUpload(file) {
+            let ajax = this.ajax;
+            let formData = new FormData();
+            console.log(file);
+            formData.append('file', file);
 
-        formData.append(this.paramName, file);
+            Object.keys(this.paramsHash || {}).forEach((key) => {
+                formData.append(key, this.paramsHash[key]);
+            });
 
-        Object.keys(this.paramsHash || {}).forEach((key) => {
-            formData.append(key, this.paramsHash[key]);
-        });
+            let response = await ajax.post(`${window.location.origin}/ghost/api/v3/admin/files/upload/`, {
+                data: formData,
+                processData: false,
+                contentType: false,
+                dataType: 'pdf',
+                xhr: () => {
+                    let xhr = new window.XMLHttpRequest();
 
-        let response = await ajax.post(`${window.location.origin}/ghost/api/v3/admin/images/upload/`, {
-            data: formData,
-            processData: false,
-            contentType: false,
-            dataType: 'text',
-            xhr: () => {
-                let xhr = new window.XMLHttpRequest();
+                    xhr.upload.addEventListener('progress', (event) => {
+                        run(() => {
+                            //tracker.update(event);
+                            //this._updateProgress();
+                        });
+                    }, false);
 
-                xhr.upload.addEventListener('progress', (event) => {
-                    run(() => {
-                        //tracker.update(event);
-                        //this._updateProgress();
-                    });
-                }, false);
-
-                return xhr;
-            }
-        });
+                    return xhr;
+                }
+            });
+        },
+        triggerFileDialog(event) {
+            // simulate click to open file dialog
+            // using jQuery because IE11 doesn't support MouseEvent
+            $(event.target)
+                .closest('.gh-setting-action')
+                .find('input[type="file"]')
+                .click();
+        },
     },
 });
